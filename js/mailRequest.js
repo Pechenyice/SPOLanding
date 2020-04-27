30296c9e66c2f0e057d784a9786db34d9e7051f6
pechenyice <german.iskhakov@mail.ru>
2020-04-27 19:37:03 +0300

    master ready top in ze world

---
+++ js/mailRequest.js
@@ -0,0 +1,25 @@
+
+document.getElementById('feedback-block-submit').onclick = (e) => {
+    e.preventDefault();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://spoconnection-rating-server.herokuapp.com/email?email=' +
+    document.getElementById('feedback-block-input').value);
+
+    // xhr.setRequestHeader('access-control-allow-origin', '*');
+
+    xhr.send();
+
+    xhr.onload = function() {
+    if (xhr.status != 200) {
+      document.getElementById('mailFailMessageBlock').style.display='flex';
+      setTimeout(() => {
+        document.getElementById('mailFailMessageBlock').style.display='none';
+      }, 5000);
+    } else {
+        document.getElementById('mailCorrectMessageBlock').style.display='flex';
+        setTimeout(() => {
+          document.getElementById('mailCorrectMessageBlock').style.display='none';
+        }, 5000);
+    }
+    };
+}
